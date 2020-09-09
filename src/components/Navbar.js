3089ee50600a50dc536f3d1cf6ed4acba9a77b64
Isabelly Dias Faustino <isabellydias29@gmail.com>
2020-09-08 21:18:22 -0300

    Alteração de cores no footer e navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,18 +10,18 @@ import logo from '../assets/logo.png';
 function Navbar() {
     return (
         <>
-            <div className="navbar">
+            <nav className="navbar">
                 <div>
                     <Link to="/"><img src={logo} alt="logo" className="logo" /></Link>
                 </div>
                 <div>
                     <div className="menu">
                         <Link className="nav-link nav-link-ltr" to="/">Home</Link>
-                       <Link className="nav-link nav-link-ltr" to="/about">Sobre</Link>
-                       <Link className="nav-link nav-link-ltr" to="/contact">Contato</Link> 
+                        <Link className="nav-link nav-link-ltr" to="/about">Sobre</Link>
+                        <Link className="nav-link nav-link-ltr" to="/contact">Contato</Link>
                     </div>
                 </div>
-            </div>
+            </nav>
         </>
     );
 }
