53d2822dc3828d25706ce6fc6e683fe66f5bf953
Isabelly Dias Faustino <isabellydias29@gmail.com>
2020-09-10 20:38:01 -0300

    Adding date in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
 import './components.css';
 
 // import { Container } from './styles';
 
-function Footer() {
+export default class Footer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.atualizarAno = { date: new Date() };
+}
+
+  render(){
   return(
       <>
       <div className="footer-dark">
         <ul>
+          <li>©{this.atualizarAno.date.getFullYear()}</li>
             <li><Link className="footer-link footer-link-ltr" to="/about">Sobre nós</Link></li>
             <li><Link className="footer-link footer-link-ltr" to="/contact">Contato</Link></li>
             <li><Link className="footer-link footer-link-ltr" to="/privacypolicy">Política de Privacidade</Link></li>
@@ -17,6 +25,5 @@ function Footer() {
       </div>
       </>
   );
+  }
 }
-
-export default Footer;
